0e01a25e60e60ff3d637a3e458ad9cb6d68a9aca
Gedeonobae <gideonobae@gmail.com>
2021-08-31 04:48:04 -0700

    that prints the last digit of a number.

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -1,4 +1,4 @@
-#include "holberton.h"
+#include "main.h"
 /**
  * print_last_digit - function that computes the absolute value of an integer
  * @c:  is the int that will use for the argument of the function
