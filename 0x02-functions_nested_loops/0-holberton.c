591ddab0c354097f1edee9b68653a2aecc556836
Gedeonobae <gideonobae@gmail.com>
2021-08-31 02:38:15 -0700

    that prints Holberton, followed by a new line.

--- 0x02-functions_nested_loops/0-holberton.c
+++ 0x02-functions_nested_loops/0-holberton.c
@@ -1,28 +1,11 @@
-/*
- * File: 0-holberton.c
- * Auth: Brennan D Baraban
- *
- */
-
-#include "holberton.h"
-
+#include <stdio.h>
 /**
- * main - Prints "Holberton" followed by a new line.
- *
- * Return: Always 0.
- */
+*main - function prints text as output
+*
+*Return:return 0
+*/
 int main(void)
 {
-	_putchar('H');
-	_putchar('o');
-	_putchar('l');
-	_putchar('b');
-	_putchar('e');
-	_putchar('r');
-	_putchar('t');
-	_putchar('o');
-	_putchar('n');
-	_putchar('\n');
-
-	return (0);
+printf("Holberton\n");
+return (0);
 }
