3503a35e885e8340e73aad66aa713c58e2653909
Gedeonobae <gideonobae@gmail.com>
2021-09-01 10:34:49 -0700

    test for a postitive_or_negative() function similar to the one you worked with in an earlier C project:

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,33 @@
+
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+* main - print if the number is postive, zero, or negative
+*
+* Description: using the main function
+* this program prints "Programming is positive, zero, or negative
+* Return: 0
+*/
+int main(void)
+{
+int n;
+
+srand(time(0));
+n = rand() - RAND_MAX / 2;
+  /* your code goes there */
+if (n > 0)
+{
+	printf("%i is positive\n", n);
+}
+else if (n == 0)
+{
+	printf("%i is zero\n", n);
+}
+else if (n < 0)
+{
+	printf("%i is negative\n", n);
+}
+return (0);
+}
