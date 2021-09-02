9a17ae0b85278a20d27061cd92f03df0460e0de0
Gedeonobae <gideonobae@gmail.com>
2021-09-01 23:41:11 -0700

    fixing comments

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -10,20 +10,14 @@ int main(void)
         int i;
 
         printf("Infinite loop incoming :(\n");
-
-        i = 0;
-
-        while (i < 10)
-        {
-                putchar(i);
-        }
-
+/**
+*        i = 0;
+*
+*        while (i < 10)
+*        {
+*                putchar(i);
+*        }
+*/	
         printf("Infinite loop avoided! \\o/\n");
 
         return (0);
-}
-
-/**
-* main - tests function that prints if integer is positive or negative
-* Return: 0
-*/
