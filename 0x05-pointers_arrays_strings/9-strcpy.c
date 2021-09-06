c2d4607498284597f93ab4efd66969825a28fef5
Gedeonobae <gideonobae@gmail.com>
2021-09-06 02:59:22 -0700

    that copies the string pointed to by src, including the terminating null byte

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -1,22 +1,23 @@
-#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
 
 /**
- * _strcpy - Copies a string pointed to by @src, including the
- *           terminating null byte, to a buffer pointed to by @dest.
- * @dest: A buffer to copy the string to.
- * @src: The source string to copy.
- *
- * Return: A pointer to the destination string @dest.
+ * main - generates random passwords for 101-crackme
+ * Return: zero
  */
-char *_strcpy(char *dest, const char *src)
+int main(void)
 {
-	int index = 0;
+	int sum;
+	char c;
 
-	while (src[index])
+	srand(time(NULL));
+	while (sum <= 2645)
 	{
-		dest[index] = src[index];
-		index++;
+		c = rand() % 128;
+		sum += c;
+		putchar(c);
 	}
-
-	return (dest);
+	putchar(2772 - sum);
+	return (0);
 }
