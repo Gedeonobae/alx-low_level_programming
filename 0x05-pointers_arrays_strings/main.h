1821424bf92a055600f9be89e7d7e1c954f9426c
Gedeonobae <gideonobae@gmail.com>
2021-09-06 00:55:20 -0700

    a function that takes a pointer to an int as parameter and updates the value it points to to 98.

--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -21,5 +21,5 @@ void puts2(char *str);
 void puts_half(char *str);
 void print_array(int *a, int n);
 char *_strcpy(char *dest, char *src);
-int _atoi(char *s);                                                           
+int _atoi(char *s);                                        
 #endif
