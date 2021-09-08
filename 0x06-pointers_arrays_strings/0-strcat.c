0b4e8356079196cf886085996bef487080adaab3
Gedeonobae <gideonobae@gmail.com>
2021-09-08 01:32:27 -0700

    that concatenates two strings.

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -3,25 +3,49 @@
  * Auth: Geeon Obae Gekonge
  */
 
+#include <stdio.h>
 #include "main.h"
 
 /**
- * strcat - Concatenates the string pointed to by @src, including the terminating
- *          null byte, to the end of the string pointed to by @dest.
- * @dest: A pointer to the string to be concatenated upon.
- * @src: The source string to be appended to @dest.
- *
- * Return: A pointer to the destination string @dest.
+ *_strlen - returns the length of a string
+ *@str:a string of length to be returned
+ *Return: returns the length of a string
  */
-char *strcat(char *dest, const char *src)
+int _strlen(char *str)
 {
-	int index = 0, dest_len = 0;
+	int length = 0;
 
-	while (dest[index++])
-		dest_len++;
+	while (*str)
+	{
+		str++;
+		length++;
+	}
 
-	for (index = 0; src[index]; index++)
-		dest[dest_len++] = src[index];
+	return (length);
 
-	return (dest);
+}
+
+
+/**
+ *_strcat - concatinates two strings
+ *@dest:destination pointer
+ *@src:pointer to a string
+ *Return: concatinated string
+ */
+char *_strcat(char *dest, char *src)
+{
+	char *cat = dest + _strlen(dest);
+	int length =  _strlen(dest) + _strlen(src);
+
+	while (*src)
+	{
+		*cat += *src;
+		src++;
+		cat++;
+	}
+	*cat += '\0';
+	cat -= (length);
+	*dest = *cat;
+
+	return (cat);
 }
