b3e1e296510e57f751aef6fcc70c69f2e4819a8b
Gedeonobae <gideonobae@gmail.com>
2021-09-08 01:12:43 -0700

    that changes all lowercase letters of a string to uppercase.

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,28 @@
+/*
+ * File: 5-string_toupper.c
+ * Auth: Gedeon Obae Gekonge
+ */
+
+#include "holberton.h"
+
+/**
+ * string_toupper - Changes all lowercase letters
+ *                  of a string to uppercase.
+ * @str: The string to be changed.
+ *
+ * Return: A pointer to the changed string.
+ */
+char *string_toupper(char *str)
+{
+	int index = 0;
+
+	while (str[index])
+	{
+		if (str[index] >= 'a' && str[index] <= 'z')
+			str[index] -= 32;
+
+		index++;
+	}
+
+	return (str);
+}
