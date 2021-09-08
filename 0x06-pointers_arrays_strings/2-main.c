84bb4445c1fd67c3a7b7860cbbfcfcf3382ab221
Gedeonobae <gideonobae@gmail.com>
2021-09-08 00:11:15 -0700

    function that copies a string.

---
+++ 0x06-pointers_arrays_strings/2-main.c
@@ -0,0 +1,18 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+    char s1[98];
+    char *ptr;
+    int i;
+
+    for (i = 0; i < 98 - 1; i++)
+    {
+        s1[i] = '*';
+    }
