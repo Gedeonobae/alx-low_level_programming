14cc4df50f644fe8ca996d199364822fa97e709b
Gedeonobae <gideonobae@gmail.com>
2021-09-10 04:24:20 -0700

    4-print_rev.c

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -1,7 +1,7 @@
 #include "main.h"
 /**
  * print_rev - func that prints a string, in reverse, followed by a new line
- * @s: declaration of s and paramters for the function
+ * @s: declaration of s and paramters for the function as in the quiz
  * Return: Always 0.
  */
 void print_rev(char *s)
