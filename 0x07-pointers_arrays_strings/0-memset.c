2bf8bf0ef6831a283bda275eec9fd1b62f239f22
Gedeonobae <gideonobae@gmail.com>
2021-09-13 02:37:47 -0700

    changes

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -3,22 +3,23 @@
  * Auth: Gedeon Obae Gekonge
  */
 
+
 #include "main.h"
+
 /**
- * _memset - fills memory with a constant byte
- * @s: memory space to be filled
- * @b: byte to fill with
- * @n: number of spaces to fill
+ * _memset -  fills memory with a constant byte.
+ * @s: string.
+ * @b: constant byte.
+ * @n: number of bytes to fill.
  *
- * Return: pointer to memory area s
+ * Return: pointer to memory area @s.
  */
 char *_memset(char *s, char b, unsigned int n)
 {
 	unsigned int i;
+	char *ptr = s;
 
-	for (i = 0; i < n; i++)
-	{
-		*(s + i) = b;
-	}
-	return (s);
+	for (i = 0; (i < n); i++)
+		s[i] = b;
+	return (ptr);
 }
