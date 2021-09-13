ada425395fea1be53e0382f951f7af851d5efa5b
Gedeonobae <gideonobae@gmail.com>
2021-09-13 02:17:04 -0700

    changes

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -3,29 +3,23 @@
  * Auth: Gedeon Obae Gekonge
  */
 
-
 #include "main.h"
-
 /**
-* _memset - fill n bytes of memory with a constant value
-* @s: pointer to memory area
-* @b: constant value
-* @n: number of bytes to fill
-* Return: memory area
-*/
-
+ * _memset - check the code for Holberton School students.
+ * @s: parameter for the array
+ * @b: parameter for the values in the array
+ * @n: parameter for the number of times to print the new values
+ * Return: Always 0.
+ */
 char *_memset(char *s, char b, unsigned int n)
 {
+	unsigned int a = 0;
 
-int size = n; /* only accept positive sizes */
+	while (a < n)
+	{
+		s[a] = b;
+		a++;
+	}
 
-if (size > 0)
-{
-int i;
-
-for (i = 0; i < size; i++)
-s[i] = b;
-}
-
-return (s);
+	return (s);
 }
