9078b8bd94a2e0d6188973102c4f822d43873231
Gedeonobae <gideonobae@gmail.com>
2021-09-13 01:06:27 -0700

    that copies memory area.

--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -4,24 +4,22 @@
  */
 
 #include "main.h"
-
 /**
- * _memcpy - Copies @n bytes from the memory area pointed
- *           to by @src into that pointed to by @dest.
- * @dest: A pointer to the memory area to copy @src into.
- * @src: The source buffer to copy characters from.
- * @n: The number of bytes to copy from @src.
- *
- * Return: A pointer to the destination buffer @dest.
+ * _memcpy - copies n bytes form the memory area src
+ * to memory area dest
+ * @src: source code to copy
+ * @dest: paramater for destination to  copy to
+ * @n: how many bytes to copy
+ * Return: dest;
  */
-void *_memcpy(void *dest, const void *src, size_t n)
+
+char *_memcpy(char *dest, char *src, unsigned int n)
 {
-	unsigned int index;
-	unsigned char *destination = dest;
-	const unsigned char *source = src;
+	unsigned int i;
 
-	for (index = 0; index < n; index++)
-		destination[index] = source[index];
-
+	for (i = 0; i < n; i++)
+	{
+		dest[i] = src[i];
+	}
 	return (dest);
 }
