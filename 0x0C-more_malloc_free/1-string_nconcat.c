5177a059dd8a7d8576d4ddd16bd64f9f41a66aeb
Gedeonobae <gideonobae@gmail.com>
2021-09-23 23:54:11 +0300

    that concatenates two strings.

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -4,46 +4,40 @@
  */
 
 #include "main.h"
-#include <stdlib.h>
+#include <stddef.h>
 
 /**
- * string_nconcat - Concatenates two strings using at
- *                  most an inputted number of bytes.
- * @s1: The first string.
- * @s2: The second string.
- * @n: The maximum number of bytes of s2 to concatenate to s1.
+ * string_nconcat - concatenates 2 strings, takes the first n chars of s2
+ * @s1: string 1
+ * @s2: string 2
+ * @n: amount of chars to be used from s2
  *
- * Return: If the function fails - NULL.
- *         Otherwise - a pointer to the concatenated space in memory.
+ * Return: pointer to the new string
  */
+
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
-	char *concat;
-	unsigned int len = n, index;
+	char *new_string;
+	int i = 0, j = 0, size_1 = strlen(s1), size_2;
 
-	if (s1 == NULL)
-		s1 = "";
-
-	if (s2 == NULL)
-		s2 = "";
-
-	for (index = 0; s1[index]; index++)
-		len++;
-
-	concat = malloc(sizeof(char) * (len + 1));
-
-	if (concat == NULL)
+	n < (unsigned int) strlen(s2) ? (size_2 = n) : (size_2 = strlen(s2));
+	new_string = (char *) malloc(size_1 + size_2 + 1);
+	if (new_string != NULL)
+	{
+		for (; i < size_1; i++)
+		{
+			new_string[i] = s1[i];
+		}
+		for (; j < size_2; j++)
+		{
+			new_string[i + j] = s2[j];
+		}
+		new_string[i + j] = '\0';
+	}
+	else
+	{
 		return (NULL);
+	}
 
-	len = 0;
-
-	for (index = 0; s1[index]; index++)
-		concat[len++] = s1[index];
-
-	for (index = 0; s2[index] && index < n; index++)
-		concat[len++] = s2[index];
-
-	concat[len] = '\0';
-
-	return (concat);
+	return (new_string);
 }
