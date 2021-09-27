e679f7fe1fa2cfebb6aca2932bcaf06d5d59f0c2
Gedeonobae <gideonobae@gmail.com>
2021-09-27 14:02:21 +0300

    that defines a macro named SIZE as an abbreviation for the token 1024.

---
+++ 0x0D-preprocessor/0-object_like_macro.h
@@ -0,0 +1,13 @@
+#ifndef OBJECT_LIKE_MACRO_H
+#define OBJECT_LIKE_MACRO_H
+
+/*
+ * File: 0-object_like_macro.h
+ * Auth: Gedeon Obae Gekonge
+ * Desc: Header file that defines a macro named
+ *       SIZE as an abbreviation for the token 1024.
+ */
+
+#define SIZE 1024
+
+#endif
