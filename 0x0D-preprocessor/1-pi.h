92d9a21873ad30f4a3259d66eb8684437b107605
Gedeonobae <gideonobae@gmail.com>
2021-09-27 14:06:47 +0300

    that defines a macro named PI as an abbreviation for the token 3.14159265359.

---
+++ 0x0D-preprocessor/1-pi.h
@@ -0,0 +1,13 @@
+#ifndef PI_H
+#define PI_H
+
+/*
+ * File: 1-pi.h
+ * Auth: Gedeon Obae Gekonge
+ * Desc: Header file that defines a macro named PI
+ *       as an abbreviation for the token 3.14159265359.
+ */
+
+#define PI 3.14159265359
+
+#endif
