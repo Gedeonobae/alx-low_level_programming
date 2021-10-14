49d829dcc20cfef904f4467c4d95d0fdb8afa55b
Gedeonobae <gideonobae@gmail.com>
2021-10-14 19:33:13 +0300

    that prints the binary representation of a number.

---
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -0,0 +1,18 @@
+/*
+ * File: 1-print_binary.c
+ * Auth: Brennan D Baraban
+ */
+
+#include "holberton.h"
+
+/**
+ * print_binary - Prints the binary representation of a number.
+ * @n: The number to be printed in binary.
+ */
+void print_binary(unsigned long int n)
+{
+	if (n > 1)
+		print_binary(n >> 1);
+
+	_putchar((n & 1) + '0');
+}
