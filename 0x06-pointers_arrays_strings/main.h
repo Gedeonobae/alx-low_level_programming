83405cf2841a7c1e122891bd3797b71b68135f1c
Gedeonobae <gideonobae@gmail.com>
2022-03-24 01:40:24 +0300

    correcting error in main.h file

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -3,7 +3,7 @@
 
 /**
  * File: holberton.h
- * Auth: Brennan D Baraban
+ * Auth: Gedeon Obae Gekonge
  * Desc: Header file containing prototypes for all functions
  *       used in the 0x05-pointers_arrays_strings directory.
  */
