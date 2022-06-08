fdbe4b11887f86d300f6f3002d674ed81b599435
Gedeon Obae Gekonge <85337093+Gedeonobae@users.noreply.github.com>
2022-06-08 05:25:06 +0300

    rename a file

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,27 @@
+/*
+ * File: 0-holberton.c
+ * Auth: Gedeon Obae Gekonge
+ *
+ */
+
+#include ""
+
+/**
+ * main - Prints "Holberton" followed by a new line.
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
+
+	return (0);
+}
