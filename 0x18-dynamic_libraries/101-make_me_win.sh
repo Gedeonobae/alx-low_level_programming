054c06b95d7d52f91d14d735a12f19a46c056efa
Bettson Kiptoo <118271406+BKiptoo@users.noreply.github.com>
2023-10-20 22:04:42 +0300

    Delete 0x18-dynamic_libraries directory

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-wget -P .. https://raw.githubusercontent.com/monoprosito/holbertonschool-low_level_programming/master/0x18-dynamic_libraries/libgiga.so
-export LD_PRELOAD="$PWD/../libgiga.so"
